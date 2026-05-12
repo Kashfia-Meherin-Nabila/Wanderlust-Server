115e0a9ae2fcf36bb60725639e1a08b16b15bb47
Kashfia Meherin <kashfiameherin2k23@gmail.com>
2026-05-12 15:20:53 +0600

    Adding the get API for getting data from the database and show them into the destination-page

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ async function run() {
     const db = client.db("wanderlust");
     const destinationCollection = db.collection("destinations");
 
+
+    app.get('/destinations', async(req, res) =>{
+      const result =await destinationCollection.find().toArray();
+      res.json(result)
+    });
+
     app.post("/destination", async (req, res) => {
       const destinationData = req.body;
       const result = await destinationCollection.insertOne(destinationData);
